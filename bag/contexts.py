de59dd026d92e05d32b82ec798cb1d15de2ff973
Davej66 <daijacobs@gmail.com>
2022-01-31 11:24:54 +0000

    Adding add to bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,8 +1,8 @@
 from decimal import Decimal
 from django.conf import settings
 
-def bag_contents(request):
 
+def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
@@ -13,9 +13,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
